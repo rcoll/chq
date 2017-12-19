bf464af261ba222b75f207729878a827d071f6b4
Rich Collier <rich.collier@automattic.com>
2017-12-19 00:39:22 -0500

    adding relay control example

---
+++ chqsat/relaycontrol.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <sr595.h>
+
+void setRelayState( int relayId, int relayState ) {
+	digitalWrite( 100 + relayId, relayState );
+}
+
+void selfTest() {
+	int i;
+
+	for ( i = 0; i < 8; i++ ) {
+		setRelayState( i, 1 );
+		delay( 250 );
+		setRelayState( i, 0 );
+		delay( 250 );
+	}
+
+	for ( i = 0; i < 8; i++ ) {
+		setRelayState( i, 1 );
+		delay( 500 );
+	}
+
+	for ( i = 0; i < 8; i++ ) {
+		setRelayState( i, 0 );
+		delay( 500 );
+	}
+
+	for ( i = 0; i < 8; i++ ) {
+		setRelayState( i, 1 );
+	}
+
+	delay( 1000 );
+
+	for ( i = 0; i < 8; i++ ) {
+		setRelayState( i, 0 );
+	}
+}
+
+int main( void ) {
+	int i;
+
+	wiringPiSetup();
+	sr595Setup( 100, 8, 0, 1, 2 );
+
+	selfTest();
+}
